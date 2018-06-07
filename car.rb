544eb945ae7737ff757d48fa8495c749b040a9a6
zoeelarson01 <zoeelarson@gmail.com>
2018-06-07 21:20:05 +0000

    automatically backed up by learn

---
+++ car.rb
@@ -0,0 +1,26 @@
+class Car
+end
+
+def initialize(make,model)
+  @make = make
+  @model = model
+  @color = white
+end
+
+def make
+  @make
+end
+
+def model
+  @model
+end
+
+def color
+  @color
+end
+
+def paint = (new_color)
+  @color = new_color
+end
+
+def initialize
